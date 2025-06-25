807bfb046254a94004666ff3b898a0d68cd4bee1
manojdx <manojmca15@gmail.com>
2025-06-25 17:56:38 +0530

    Update MainActivity.kt

--- app/src/main/java/com/acsen/samplegit/MainActivity.kt
+++ app/src/main/java/com/acsen/samplegit/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SampleGitTheme {
-        Greeting("Android Manoj")
+        Greeting("Android Manoj Test")
     }
 }
